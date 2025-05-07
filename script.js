7a557ffb52fe7ba3de6c7ba6578b37dfa9a63253
yunus103 <yunusemre.aytekin@ogr.iuc.edu.tr>
2025-05-07 17:00:02 +0700

    Validation added to input box to make sure it can only have digits and operations and no more than 1 operation

--- script.js
+++ script.js
@@ -1,5 +1,75 @@
 let number1, number2, oprt;
 
+const displayBox = document.querySelector("#display-box");
+const buttons = document.querySelectorAll(".buttons");
+const btnClear = document.querySelector("#clear");
+const allowedKeys = [
+    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+    "+", "-", "*", "/", "%",".", "(", ")", "Backspace", "Delete", "ArrowLeft", "ArrowRight", "Enter"
+  ];
+
+const operations = ["+", "-", "*", "x", "/", "%"];
+
+function hasOperator(value) {
+    return operations.some(op => value.includes(op));
+}
+
+displayBox.addEventListener("keydown", function(e){
+
+    if(!allowedKeys.includes(e.key)){
+        e.preventDefault();
+        return;
+    }
+
+    if (["+", "-", "*", "/", "%"].includes(e.key)) {
+        if (hasOperator(displayBox.value)) {
+        e.preventDefault();
+        return;
+        }
+    }
+
+    // e.preventDefault(); // always prevent default operator
+    if (e.key === "/") {
+        e.preventDefault();
+      displayBox.value += "÷";
+    } else if (e.key === "*") {
+        e.preventDefault();
+      displayBox.value += "x";
+    } else if(["Backspace", "Delete", "ArrowLeft", "ArrowRight", "Enter"].includes(e.key)){
+    } 
+    else {
+        e.preventDefault();
+      displayBox.value += e.key;
+    }
+
+    return;
+});
+
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const type = button.dataset.type;
+
+        
+        
+        if(type === "digit"){
+            displayBox.value += button.textContent;
+        } else if(type === "op"){
+            if (hasOperator(displayBox.value)) {
+                e.preventDefault();
+            }else{
+                displayBox.value += button.textContent;
+            }
+        }else if(type === "clear") {
+            displayBox.value  = "";
+        } else if(type === "equals"){
+            // Equal
+        } else if(type === "delete"){
+
+        }
+    });
+});
+
 
 function operate(op, num1, num2){
     if (op === "÷") {
